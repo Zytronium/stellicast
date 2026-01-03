3ee9e9cbf604c0035f0e17c4d4477304ae581828
Zytronium <dastelljes@outlook.com>
2026-01-02 20:51:16 -0600

    Fix profile picture and banner upload

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'share.zytronium.dev',
       },
+      {
+        protocol: 'https',
+        hostname: 'bfbgxztphnbtrlplityh.supabase.co',
+      },
     ],
   },
   reactCompiler: true,
